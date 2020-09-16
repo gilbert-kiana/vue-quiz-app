4db9aa7b8a4ae2ec3237fb05c488e2c4f75039de
gilbert-kiana <gilbertintabo@gmail.com>
2020-09-16 15:01:56 +0300

    started with just 2 dummy questions no CSS yet

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,70 @@
 <template>
-  <div id="app">
-  <Header msg="Quiz app"/>
+  <div class="container-app">
+    <div class="container-quiz">
+      <div class="quiz-header">
+           <h1>QUIZ APP</h1>
+      </div>
+      <div class="quiz-main" v-for="(element,index) in questions" :key="index">
+          <div class="the-questions">
+           <h1>Questions</h1>
+           <p>{{element.question}}</p>
+          </div>
+          <div class="answers">
+            <ul>
+              <li v-for="(item,index) in element.options" :key="index">
+                {{item.option}}
+              </li>
+            </ul>
+          </div>
+      </div>
+      <div class="quiz-footer">
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
 
 export default {
   name: 'App',
-  components: {
-    Header
+  data(){
+    return{
+      questions:[
+        {
+          question:'Whose the writer of this program.',
+          options:[
+            {option:'kaiser',correct:true},
+            {option:'kiana'},
+            {option:'gilbert'},
+            {option:'intabo'} 
+          ]
+        },
+        {
+          question:'Which is my favorite school',
+          options:[
+            {option:'maseno',correct:true},
+            {option:'alliance'},
+            {option:'lenana'},
+            {option:'orero'} 
+          ]
+        },
+        {
+          question:'',
+          options:[
+            {option:''},
+            {option:''},
+            {option:''},
+            {option:''} 
+          ]
+        }
+      ]
+    }
   }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
